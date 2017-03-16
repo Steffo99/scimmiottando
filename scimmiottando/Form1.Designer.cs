cd2763a72a2cc692bd02fe4b47240bad814836cd
Stefano Pigozzi <ste.pigozzi@gmail.com>
2017-03-16 12:17:27 +0100

    Volevo fare più commit invece ne ho fatto uno solo
    Eh vabbè

--- scimmiottando/Form1.Designer.cs
+++ scimmiottando/Form1.Designer.cs
@@ -29,11 +29,50 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBoxStart = new System.Windows.Forms.PictureBox();
+            this.timerShow = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxStart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxStart
+            // 
+            this.pictureBoxStart.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxStart.Image")));
+            this.pictureBoxStart.Location = new System.Drawing.Point(12, 272);
+            this.pictureBoxStart.Name = "pictureBoxStart";
+            this.pictureBoxStart.Size = new System.Drawing.Size(32, 32);
+            this.pictureBoxStart.TabIndex = 0;
+            this.pictureBoxStart.TabStop = false;
+            this.pictureBoxStart.Click += new System.EventHandler(this.pictureBoxStart_Click);
+            // 
+            // timerShow
+            // 
+            this.timerShow.Interval = 500;
+            this.timerShow.Tick += new System.EventHandler(this.timerShow_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(284, 316);
+            this.Controls.Add(this.pictureBoxStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxStart)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxStart;
+        private System.Windows.Forms.Timer timerShow;
     }
 }
 
--- scimmiottando/Form1.cs
+++ scimmiottando/Form1.cs
@@ -12,9 +12,125 @@ namespace scimmiottando
 {
     public partial class Form1 : Form
     {
+        Label[][] squares = new Label[5][];
+        int[] numbers = new int[5];
+        int step = 0;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public void Reset()
+        {
+            numbers = new int[5];
+            for (int x = 0; x < 5; x++)
+            {
+                for (int y = 0; y < 5; y++)
+                {
+                    squares[x][y].Text = " ";
+                    squares[x][y].Tag = " ";
+                }
+            }
+            step = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Initialize the grid
+            for (int x = 0; x < 5; x++)
+            {
+                squares[x] = new Label[5];
+                for (int y = 0; y < 5; y++)
+                {
+                    Label l = new Label();
+                    squares[x][y] = l;
+                    //Init the label
+                    l.AutoSize = false;
+                    l.Font = new System.Drawing.Font("Arial Black", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                    l.ForeColor = System.Drawing.Color.White;
+                    l.Location = new System.Drawing.Point(13 + x * 53, 13 + y * 53);
+                    l.MaximumSize = new System.Drawing.Size(40, 40);
+                    l.MinimumSize = new System.Drawing.Size(40, 40);
+                    l.Name = "labelx" + x.ToString() + "y" + y.ToString();
+                    l.Size = new System.Drawing.Size(40, 40);
+                    l.Text = " ";
+                    l.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+                    l.Click += square_Click;
+                    Controls.Add(l);
+                }
+            }
+            ResumeLayout();
+            PerformLayout();
+        }
+
+        private void pictureBoxStart_Click(object sender, EventArgs e)
+        {
+            Reset();
+            //Generate random numbers
+            Random generator = new Random();
+            for (int c = 0; c < 5; c++)
+            {
+                bool successful = false;
+                while (!successful)
+                {
+                    int x = generator.Next(5);
+                    int y = generator.Next(5);
+                    numbers[c] = -1;
+                    bool validnumber = false;
+                    while (!validnumber)
+                    {
+                        int n = generator.Next(10);
+                        validnumber = !numbers.Contains(n);
+                        numbers[c] = n;
+                    }
+                    if(squares[x][y].Text == " ")
+                    {
+                        squares[x][y].Text = numbers[c].ToString();
+                        squares[x][y].Tag = numbers[c].ToString();
+                        successful = true;
+                    }
+                }
+            }
+            //Sort the numbers array
+            Array.Sort<int>(numbers);
+            //Start the timer
+            timerShow.Start();
+        }
+
+        private void timerShow_Tick(object sender, EventArgs e)
+        {
+            for (int x = 0; x < 5; x++)
+            {
+                for (int y = 0; y < 5; y++)
+                {
+                    if(squares[x][y].Text != " ")
+                    {
+                        squares[x][y].Text = "█";
+                    }
+                }
+            }
+            timerShow.Stop();
+        }
+
+        private void square_Click(object sender, EventArgs e)
+        {
+            Label square = sender as Label;
+            if(square.Tag.ToString() == numbers[step].ToString())
+            {
+                square.Text = " ";
+                step += 1;
+            }
+            else if(square.Text != " ")
+            {
+                MessageBox.Show("Hai sbagliato!");
+                Reset();
+            }
+            if(step >= 5)
+            {
+                MessageBox.Show("Hai vinto!");
+                Reset();
+            }
         }
     }
 }
